976521ee95a827e9dd41ab1b3292ece097506dda
Bashir Manchala <bashirmanchala@KAMINT.com>
2023-04-09 13:14:18 +0500

    save changes

---
+++ Assignment1/app.js
@@ -0,0 +1,22 @@
+// Q1. Write a script to greet your website visitor using JS alert
+// box.
+// Q2. Write a script to display following message on your web
+// page:
+// alert("Error! Please enter a valid password.");
+
+// Q3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+// Q4. Write a script to display following messages in sequence:
+// alert("Welcome to JS Land...");
+// var oCk = {cMsg:"Prevent this page from creating additional dialogs", bInitialValue:true, bAfterValue:false};
+// alert("Happy Coding!,nIkon:3, Checkbox:oCK");
+
+// Q5. Generate the following message through browser’s
+// developer console:
+//  alert("Hello...I can run JS through my browser's console. ");
+// First, setup the Check Box object literal
+var oCk = {cMsg:"Yes, I want to get the newsletter", bInitialValue:true, bAfterValue:false};
+// Display Alert box
+alert({cMsg:"Thank you for filling out the form", nIcon:3, oCheckbox:oCk})
